052a1dee674d60b7fcff5db6e8b2147a88cbbda6
dejetem <enyeoluwarotimisimon@gmail.com>
2021-01-04 00:38:44 +0100

    node app

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ const videoGrid = document.getElementById('video-grid')
 const peer = new Peer(undefined, {
   path: '/peerjs',
   host: '/',
-  port: '8000'
+  port: '443'
 })
 let myVideoStream;
 const myVideo = document.createElement('video')
